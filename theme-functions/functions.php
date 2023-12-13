b11d3f092d2e51c297dfae019955b194fee44a98
Nico Watine <nico@studioncreations.com>
2023-12-13 12:50:33 -0600

    Add annotation

--- theme-functions/functions.php
+++ theme-functions/functions.php
@@ -37,7 +37,7 @@ remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
 remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
 
 
-
+// Additional functions to be annotated
 remove_action('wp_head', 'index_rel_link');
 remove_action('wp_head', 'start_post_rel_link', 10, 0);
 remove_action('wp_head', 'parent_post_rel_link', 10, 0);
@@ -48,4 +48,4 @@ remove_action( 'wp_head', 'wp_resource_hints', 2, 99 );
 remove_action('rest_api_init', 'wp_oembed_register_route');
 remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);
 remove_filter('pre_oembed_result', 'wp_filter_pre_oembed_result', 10);
-add_filter('embed_oembed_discove
+add_filter('embed_oembed_discover', '__return_false');
