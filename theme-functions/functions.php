5944d372a2f1fd1c23b772c96b05c43a6e679ef8
Nico Watine <nico@studioncreations.com>
2023-12-13 12:46:34 -0600

    More annotations

--- theme-functions/functions.php
+++ theme-functions/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-// Avada function
+// Avada function - load Child Theme CSS
 function theme_enqueue_styles() {
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [] );
 }
@@ -13,32 +13,39 @@ function avada_lang_setup() {
 }
 add_action( 'after_setup_theme', 'avada_lang_setup' );
 
-// Remove "This site is optimized with Yoast" in <head>
+// Remove "This site is optimized with Yoast"
 add_filter( 'wpseo_debug_markers', '__return_false' );
 
 // Remove <meta name="generator" content="WordPress 6.4.2" />
 remove_action('wp_head', 'wp_generator');
 
-// Custom function Additions
+// Remove the WordPress shortlink for your page/post
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+
+// Remove rsd link
 remove_action('wp_head', 'rsd_link');
+
+// Remove RSS feed links
 remove_action('wp_head', 'feed_links', 2);
 remove_action('wp_head', 'feed_links_extra', 3);
-remove_action('wp_head', 'index_rel_link');
+
+// Remove the link to the Windows Live Writer manifest file
 remove_action('wp_head', 'wlwmanifest_link');
-remove_action('wp_head', 'start_post_rel_link', 10, 0);
-remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+
+// Removes the adjacent post links
 remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
 remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
-remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+
+
+
+remove_action('wp_head', 'index_rel_link');
+remove_action('wp_head', 'start_post_rel_link', 10, 0);
+remove_action('wp_head', 'parent_post_rel_link', 10, 0);
 remove_action('wp_head', 'wp_oembed_add_discovery_links');
 remove_action('wp_head', 'wp_oembed_add_host_js');
 remove_action('wp_head', 'rest_output_link_wp_head');
 remove_action( 'wp_head', 'wp_resource_hints', 2, 99 );
-
 remove_action('rest_api_init', 'wp_oembed_register_route');
-
-
 remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);
 remove_filter('pre_oembed_result', 'wp_filter_pre_oembed_result', 10);
-
 add_filter('embed_oembed_discover', '__return_false');
