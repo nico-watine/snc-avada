038b07188de330f95896c210ba9dcf7021bc6b6d
Nico Watine <nico@studioncreations.com>
2023-12-13 12:27:19 -0600

    Add annotation

--- theme-functions/functions.php
+++ theme-functions/functions.php
@@ -1,10 +1,12 @@
 <?php
 
+// Avada function
 function theme_enqueue_styles() {
 		wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [] );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', 20 );
 
+// Avada funtion
 function avada_lang_setup() {
 	$lang = get_stylesheet_directory() . '/languages';
 	load_child_theme_textdomain( 'Avada', $lang );
